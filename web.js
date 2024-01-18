3a7a5f55638e42d554952c6ec04dfeb66676d6bd
zhiqiang <luzhiqiangbj@163.com>
2024-01-18 16:08:43 +0800

    ll

---
+++ web.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  response.write('hello');
+  response.end();
+});
+
+server.listen(3000, () => {
+  console.log('服务已经启动');
+});
